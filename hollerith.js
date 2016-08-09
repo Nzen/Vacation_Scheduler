5125c827e9d793b5a776caed18aba4883f772a2e
Nicholas Prado <nmprado@nzen.ws>
2016-08-08 22:59:24 -0500

    Done with prototypes but it doesn't work
    Adding functions to the object prototypes instead of the constructor
    function. Now VsApp doesn't know the type of the people it holds
    when I try to get a string version.

--- hollerith.js
+++ hollerith.js
@@ -43,154 +43,154 @@ termin.log = function( message )
 /** a list of days; callee handles preference level */
 function Choice()
 {
-	this.days = [],
-	addDate: function( anotherDay )
+	this.days = [];
+};
+Choice.prototype.addDate = function( anotherDay )
+{
+	var ind = this.days.indexOf( anotherDay );
+	if ( ind < 0 )
 	{
-		var ind = this.days.indexOf( anotherDay );
-		if ( ind < 0 )
-		{
-			this.days.push( anotherDay );
-		}
-	},
-	removeDate: function( badDay )
-	{
-		var ind = this.days.indexOf( anotherDay );
-		if ( ind >= 0 )
-		{
-			this.days.splice( ind, 1 );
-			// REM remove that element, reindex array
-		}
-	},
-	getDays: function()
-	{
-		return this.days;
-	},
-	numOfDays: function()
-	{
-		return this.days.length;
-	},
-	asString: function()
-	{
-		var desc = this.level.toString();
-		for ( var aDay in this.days )
-		{
-			desc += ", "+ aDay.toString();
-		}
-		return desc;
+		this.days.push( anotherDay );
 	}
+};
+Choice.prototype.removeDate = function( badDay )
+{
+	var ind = this.days.indexOf( anotherDay );
+	if ( ind >= 0 )
+	{
+		this.days.splice( ind, 1 );
+		// REM remove that element, reindex array
+	}
+};
+Choice.prototype.getDays = function()
+{
+	return this.days;
+};
+Choice.prototype.numOfDays = function()
+{
+	return this.days.length;
+};
+Choice.prototype.asString = function()
+{
+	var desc = this.level.toString();
+	for ( var aDay in this.days )
+	{
+		desc += ", "+ aDay.toString();
+	}
+	return desc;
 };
 
 /** an ordered list of Choices; callee handles seniority */
 function Person( theName )
 {
 	this.nameId = theName,
-	this.preferences = [],
-	addToChoice: function( aDay, level )
+	this.preferences = [];
+};
+Person.prototype.addToChoice = function( aDay, level )
+{
+	if ( level > this.preferences.length
+		|| this.preferences[ level ] == undefined )
 	{
-		if ( level > this.preferences.length
-			|| this.preferences[ level ] == undefined )
-		{
-			this.preferences[ level ] = new Choice();
-		}
-		this.preferences[ level ].addDate( aDay );
-	},
-	removeFromChoice( aDay, level )
-	{
-		if ( this.preferences[ level ] != undefined )
-			this.preferences[ level ].removeDate( aDay );
-	},
-	/** value or undefined; null means you're past the limit */
-	getChoice: function( level )
-	{
-		if ( level > this.preferences.length )
-			return null;
-		else
-			return this.preferences[ level ];		
-	},
-	/** the highest value choice, not how many are allocated */
-	numOfChoices: function()
-	{
-		return this.preferences.length;
-	},
-	asString: function()
-	{
-		var pDesc = this.nameId;
-		for ( var desire in this.preferences )
-		{
-			pDesc += "\n"+ desire.asString();
-		}
-		return desc;
+		this.preferences[ level ] = new Choice();
 	}
+	this.preferences[ level ].addDate( aDay );
+};
+Person.prototype.removeFromChoice = function( aDay, level )
+{
+	if ( this.preferences[ level ] != undefined )
+		this.preferences[ level ].removeDate( aDay );
+};
+/** value or undefined; null means you're past the limit */
+Person.prototype.getChoice = function( level )
+{
+	if ( level > this.preferences.length )
+		return null;
+	else
+		return this.preferences[ level ];		
+};
+/** the highest value choice, not how many are allocated */
+Person.prototype.numOfChoices = function()
+{
+	return this.preferences.length;
+}
+Person.prototype.asString = function()
+{
+	var pDesc = this.nameId;
+	for ( var desire in this.preferences )
+	{
+		pDesc += "\n"+ desire.asString();
+	}
+	return desc;
 };
 
 /** holds all the people entered currently */
 function VsModel()
 {
-	this.people = [],
-	/** add choice, conflict pushes original to the bottom */
-	addChoiceToPerson: function( nameEntered, seniority, priority, day )
+	this.people = [];
+};
+/** add choice, conflict pushes original to the bottom */
+VsModel.prototype.addChoiceToPerson = function( nameEntered, seniority, priority, day )
+{
+	if ( seniority > this.people.length
+		|| this.people[ seniority ] == undefined )
 	{
-		if ( seniority > this.people.length
-			|| this.people[ seniority ] == undefined )
-		{
-			this.people[ seniority ] = new Person( nameEntered );
-		}
-		else if ( this.people[ seniority ].nameId != nameEntered )
-		{
-			// send it to the back
-			var tempPerson = new Person( this.people[ seniority ].nameId );
-			for ( var ind = 0; ind < this.people[ seniority ].numOfChoices();
-				ind++ )
-			{
-				var prevChoiceOfLevel = this.people[ seniority ].getChoice( ind );
-				for ( var aDay in prevChoiceOfLevel )
-				{
-					tempPerson.addToChoice( aDay, ind );
-				}
-			}
-			this.people.push( tempPerson );
-			// now add the new person in his'oer place
-			this.people[ seniority ] = new Person( nameEntered );
-		}
-		this.people[ seniority ].addToChoice( day, priority );
-	},
-	removeChoiceFromPerson: function( nameEntered, seniority, priority, day )
+		this.people[ seniority ] = new Person( nameEntered );
+	}
+	else if ( this.people[ seniority ].nameId != nameEntered )
 	{
-		if ( seniority > this.people.length
-			|| this.people[ seniority ] == undefined )
+		// send it to the back
+		var tempPerson = new Person( this.people[ seniority ].nameId );
+		for ( var ind = 0; ind < this.people[ seniority ].numOfChoices();
+			ind++ )
 		{
-			this.people[ seniority ] = new Person( nameEntered );
+			var prevChoiceOfLevel = this.people[ seniority ].getChoice( ind );
+			for ( var aDay in prevChoiceOfLevel )
+			{
+				tempPerson.addToChoice( aDay, ind );
+			}
 		}
-		else if ( this.people[ seniority ].nameId != nameEntered )
+		this.people.push( tempPerson );
+		// now add the new person in his'oer place
+		this.people[ seniority ] = new Person( nameEntered );
+	}
+	this.people[ seniority ].addToChoice( day, priority );
+};
+/** remove a choice from a person */
+VsModel.prototype.removeChoiceFromPerson = function( nameEntered, seniority, priority, day )
+{
+	if ( seniority > this.people.length
+		|| this.people[ seniority ] == undefined )
+	{
+		this.people[ seniority ] = new Person( nameEntered );
+	}
+	else if ( this.people[ seniority ].nameId != nameEntered )
+	{
+		var indOfPerson = this.people.indexOf( nameEntered );
+		if ( indOfPerson < 0 )
 		{
-			var indOfPerson = this.people.indexOf( nameEntered );
-			if ( indOfPerson < 0 )
-			{
-				this.people.push(new Person( nameEntered ));
-			}
-			else
-			{
-				this.people[ ind ].removeFromChoice( day, priority );
-			}
+			this.people.push(new Person( nameEntered ));
 		}
 		else
 		{
-				this.people[ ind ].removeFromChoice( day, priority );
+			this.people[ indOfPerson ].removeFromChoice( day, priority );
 		}
 	}
-	asString: function()
+	else
 	{
-		var theEverything = "";
-		var ind = 0;
-		for ( var who in this.people )
-		{
-			theEverything += ind.toString()
-					+" "+ who.asString(); 
-		}
-		return theEverything;
+			this.people[ indOfPerson ].removeFromChoice( day, priority );
 	}
-	// IMPROVE other getters for constrainer, or will it access them directly?
 };
+/** print everything within the VsModel */
+VsModel.prototype.asString = function()
+{
+	var theEverything = "";
+	for ( var ind = 0; ind < this.people.length; ind++ )
+	{
+		theEverything += ind.toString() +" "+ this.people[ ind ].asString(); 
+	}
+	return theEverything;
+};
+	// IMPROVE other getters for constrainer, or will it access them directly?
 
 // --- dom interface
 
@@ -199,7 +199,7 @@ var peoples = new VsModel();
 function addChoice()
 {
 	termin.log( "add choice" );
-	peoples.removeChoiceFromPerson(
+	peoples.addChoiceToPerson(
 			document.getElementById("name").value,
 			document.getElementById("senority").value,
 			document.getElementById("preference").value,
@@ -221,7 +221,7 @@ day <input type="text" id="day" maxlength="2" size="5" />
 function removeChoice()
 {
 	termin.log( "remove choice" );
-	peoples.addChoiceToPerson(
+	peoples.removeChoiceFromPerson(
 			document.getElementById("name").value,
 			document.getElementById("senority").value,
 			document.getElementById("preference").value,
