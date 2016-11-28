6d62aba946d52f9d341486f83ccadb0a9165d10e
Nicholas Prado <nmprado@nzen.ws>
2016-11-27 20:19:20 -0600

    Goes with the last commit
    Struct of the ui values

---
+++ src/ws/nzen/vacay/model/CalendarUi.java
@@ -0,0 +1,73 @@
+
+package ws.nzen.vacay.model;
+
+import java.io.Serializable;
+import java.time.YearMonth;
+
+/** struct for saving and restoring ui entries */
+public class CalendarUi implements Serializable
+{
+
+	private static final long serialVersionUID = 1L;
+	private YearMonth currentMonth;
+	private String personName;
+	private String seniorityVal;
+	private String desireVal;
+	private boolean radioAdd;
+
+	public CalendarUi( YearMonth shownYearMonth, String name,
+			String seniorness, String desire, boolean isAdd )
+	{
+		currentMonth = shownYearMonth;
+		personName = name;
+		seniorityVal = seniorness;
+		desireVal = desire;
+		radioAdd = isAdd;
+	}
+
+	public YearMonth getCurrentMonth()
+	{
+		return currentMonth;
+	}
+	public void setCurrentMonth( YearMonth currentMonth )
+	{
+		this.currentMonth = currentMonth;
+	}
+	public String getPersonName()
+	{
+		return personName;
+	}
+	public void setPersonName( String personName )
+	{
+		this.personName = personName;
+	}
+	public String getSeniorityVal()
+	{
+		return seniorityVal;
+	}
+	public void setSeniorityVal( String seniorityVal )
+	{
+		this.seniorityVal = seniorityVal;
+	}
+	public String getDesireVal()
+	{
+		return desireVal;
+	}
+	public void setDesireVal( String desireVal )
+	{
+		this.desireVal = desireVal;
+	}
+	public boolean isRadioAdd()
+	{
+		return radioAdd;
+	}
+	public void setRadioAdd( boolean radioAdd )
+	{
+		this.radioAdd = radioAdd;
+	}
+	public static long getSerialversionuid()
+	{
+		return serialVersionUID;
+	}
+
+}
